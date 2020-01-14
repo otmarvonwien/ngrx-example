4ff72e3116dab03e8113f1024b635e6be4a49a9c
Otmar Braunecker <otmarvonwien@hotmail.com>
2020-01-14 12:00:41 +0100

    added clear error action and set loading true (for spinner) at signup start

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -27,6 +27,7 @@ export class AuthComponent implements OnInit, OnDestroy {
   @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
 
   private closeSub: Subscription;
+  private storeSub: Subscription;
 
   constructor(
     private authService: AuthService,
@@ -36,7 +37,7 @@ export class AuthComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this.store.select('auth').subscribe(authState => {
+    this.storeSub = this.store.select('auth').subscribe(authState => {
       this.isLoading = authState.loading;
       this.error = authState.authError;
       if (this.error) {
@@ -68,12 +69,15 @@ export class AuthComponent implements OnInit, OnDestroy {
   }
 
   onHandleError() {
-    this.error = null;
+    this.store.dispatch(new AuthActions.ClearError());
   }
 
   ngOnDestroy() {
     if (this.closeSub) {
       this.closeSub.unsubscribe();
+    }
+    if (this.storeSub) {
+      this.storeSub.unsubscribe();
     }
   }
 
@@ -89,6 +93,7 @@ export class AuthComponent implements OnInit, OnDestroy {
 
     componentRef.instance.message = message;
     this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.onHandleError();
       this.closeSub.unsubscribe();
       hostViewContainerRef.clear();
     });
