6880cd3382be63d64bb564b15da631ab587483d3
Otmar Braunecker <otmarvonwien@hotmail.com>
2020-01-14 19:34:57 +0100

    added fetch recipes effect and action and dispatch it from the header component and the recipes resolver

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -4,25 +4,27 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot
 } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Actions, ofType } from '@ngrx/effects';
+import { take } from 'rxjs/operators';
 
 import { Recipe } from './recipe.model';
-import { DataStorageService } from '../shared/data-storage.service';
-import { RecipeService } from './recipe.service';
+import * as fromApp from '../store/app.reducer';
+import * as RecipeActions from '../recipes/store/recipe.actions';
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
   constructor(
-    private dataStorageService: DataStorageService,
-    private recipesService: RecipeService
+    private store: Store<fromApp.AppState>,
+    private actions$: Actions
   ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const recipes = this.recipesService.getRecipes();
-
-    if (recipes.length === 0) {
-      return this.dataStorageService.fetchRecipes();
-    } else {
-      return recipes;
-    }
+    // return this.dataStorageService.fetchRecipes();
+    this.store.dispatch(new RecipeActions.FetchRecipes());
+    return this.actions$.pipe(
+      ofType(RecipeActions.SET_RECIPES),
+      take(1)
+    );
   }
 }
---
+++ src/app/recipes/store/recipe.effects.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { switchMap, map } from 'rxjs/operators';
+
+import * as RecipeActions from './recipe.actions';
+import { Recipe } from '../recipe.model';
+
+@Injectable()
+export class RecipeEffects {
+  @Effect()
+  fetchRecipes = this.actions$.pipe(
+    ofType(RecipeActions.FETCH_RECIPES),
+    switchMap(() => {
+      return this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
+      );
+    }),
+    map(recipes => {
+      return recipes.map(recipe => {
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients ? recipe.ingredients : []
+        };
+      });
+    }),
+    map(recipes => {
+      return new RecipeActions.SetRecipes(recipes);
+    })
+  );
+
+  constructor(private actions$: Actions, private http: HttpClient) {}
+}
